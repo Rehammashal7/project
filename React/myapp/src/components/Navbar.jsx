82b4302cbbc3109ca21c296bebb7ac8b4f409302
walaa244 <walaaesmail91@gmail.com>
2022-12-25 14:53:44 +0200

    new

---
+++ React/myapp/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height : 30px;
+  background-color: yellow;
+`
+ 
+  
+
+
+const Navbar = () => {
+  return (
+    <Container>
+      zrfxfd
+    </Container>
+  )
+}
+
+export default Navbar
