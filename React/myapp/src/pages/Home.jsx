4e3a5f4cbbfa7fcd8b6d916f53ba1012b69058c0
Rehammasshal7 <rehammashal7@gmail.com>
2022-12-28 09:01:21 +0200

    .

--- React/myapp/src/pages/Home.jsx
+++ React/myapp/src/pages/Home.jsx
@@ -1,18 +1,13 @@
+
 import React from 'react'
-import Announcement from '../components/Announcement'
-import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import Newsletter from '../components/Newsletter'
-import Products from '../components/Products'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
-      <Announcement/>
       <Navbar/>
-      <Products/>
-      <Newsletter/>
-      <Footer/>
+     <Slider/>
     </div>
   )
 }
