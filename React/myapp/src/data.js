da1f933c58e9886f4693aeca5d48b5c503a66f53
Habiba <soso2006bibo@gmail.com>
2022-12-28 21:20:21 +0200

    data

--- React/myapp/src/data.js
+++ React/myapp/src/data.js
@@ -1,46 +1,79 @@
 export const popularProducts = [
     {
       id:1,
-      img:"https://m.media-amazon.com/images/I/41xGWWaJjML.__AC_SX300_SY300_QL70_ML2_.jpg",
+      img:"https://www.laroche-posay.me/-/media/project/loreal/brand-sites/lrp/apac/mena/products/toleriane/sensitive-prebiotic-moisturizer/la-roche-posay-productpage-sensitive-allergic-toleriane-sensitive-prebiotic-40ml-3337875578486-front.png",
+      // titel:"TOLERIANE SENSITIVE CREAM",
+      // dis:"TOLERIANE SENSITIVE CREAM soothes, protects and moisturises. A dermatological breakthrough in skin hydration. Safe, even for babies.",
+      // price:"200$",
     },
     {
       id:2,
-      img:"https://m.media-amazon.com/images/I/414QoetjjVL.__AC_SX300_SY300_QL70_ML2_.jpg",
+      img:"https://www.bathandbodyworks.com.eg/assets/styles/bbw/image-thumb__268302__product_zoom_medium_606x504/26240397_1.jpg",
+      // titel:"INTO THE NIGHT"
+      // dis:"Evoke natural confidence & brilliant beauty from the beginning of your day Into the Night. Our dermatologist-tested Shower Gel combines nourishing vitamin E & skin-loving shea butter to create a rich, bubbly lather."
+      // price:"210$"
     },
     {
       id:3,
       img:"https://cdn.shopify.com/s/files/1/0601/5438/0518/products/acnecleanser_700x.jpg?v=1671028973",
+      // titel:"Starville facial cleanser gel ",
+      // dis:"Daily hygiene cleanser for combination, oily and acne prone skin,and Regulates sebum production, gently cleanses and purifies the skin from excessive sebum and impurities.",
+      // price:"100$",
     },
     {
       id:4,
       img:" https://www.bathandbodyworks.com.eg/assets/styles/bbw/image-thumb__10299__product_zoom_medium_606x504/24642412_1.jpg",
+      // titel:"JAPANESE CHERRY BLOSSOM",
+      // dis:"One of America's best-selling fragrances, Japanese Cherry Blossom is your little black dress fragrance: beautiful, timeless & undeniably feminine. Nourishing vitamin E combines with skin-loving shea butter & aloe in this rich, bubbly lather. It's the perfect daily cleanser to leave skin feeling fresh & clean!",
+      // price:"500$",
     },
     {
       id:5,
-      img:"https://m.media-amazon.com/images/I/51odhDWZvFL.__AC_SX300_SY300_QL70_ML2_.jpg",
+      img:"https://www.laroche-posay.me/-/media/project/loreal/brand-sites/lrp/apac/mena/products/toleriane/dermo-cleanser/la-roche-posay-productpage-sensitive-allergic-toleriane-dermo-cleanser-400ml-3337872411830-front.png",
+      // titel:"DERMO-CLEANSER",
+      // dis:"Facial cleanser and make-up remover. Can be rinsed off or removed with cotton or tissue.",
+      // price:"400",
     },
     {
       id:6,
       img:"https://cdn.shopify.com/s/files/1/0601/5438/0518/products/Shaan-Body-Milk-300ml-_1_aef234e4-1b53-4831-9a68-9a2b5b024ec0_700x.png?v=1665065109",
+      // titel:"Shaan Body Milk"
+      // dis:"Body lotion for dry skin Deep hydration for the body that lasts 72 hours , contains ceramides and hyaluronic acid Vit B3 and Vit E. Moisturizing gives you soothing and nourishing properties due to its special formula enriched with Shea Butter . Allows long - lasting hydration of the upper layers . How to use : Apply daily on clean dry skin as desired especially after shower ."
+      // price:'200$'
     },
     {
       id:7,
       img:"https://floward.imgix.net/web/Files/thumPro/c41c7d3b-de7d-4a47-9002-c8c7b9aaa296.jpg?w=600&auto=format",
+      // titel:"Joviality Set VII",
+      // dis:"The arrangement contains Natural Body Lotion and Nail growth.",
+      // price:"450$",    
     },
     {
       id:8,
       img:"https://cdn.shopify.com/s/files/1/0601/5438/0518/products/044_a7099f72-da4e-4c2f-9258-e51f7458b051_600x.png?v=1666193660",
+      // titel:"Shaan Rejuvenation Cream",
+      // dis:"Shaan medical moisturizing cream contains many natural ingredients, as it contains 12 ingredients that moisturize and nourish dry skin. It also works to renew skin cells, soothe skin redness and treat skin irritations. Shaan cream also contains ingredients that protect the skin from the formation of bacteria on The skin and makes the skin moist, clean and supple. Chan cream is suitable for dry, normal and sensitive skin.",
+      // price:"150$",
     },
     {
       id:9,
       img:"https://www.bathandbodyworks.com.eg/assets/styles/bbw/image-thumb__192677__product_zoom_medium_606x504/26357104_1.jpg",
+    // titel:"IN THE STARS"
+    // dis:"Make every moment glow with an addictive fragrance that radiates from start to finish like the stars above. Our dermatologist-tested Shower Gel combines nourishing vitamin E & aloe to create a rich, bubbly lather. Gentle & non-drying, it's the perfect daily cleanser to leave skin feeling fresh & clean!"
+    // price:"470$"
     },
     {
       id:10,
       img:"https://www.kerastase.com.eg/media/catalog/product/cache/c26b1bb45d7ad8f43995e8ff61fc8a17/k/e/kerastase-resistance-therapiste-masque-hair-mask.jpg",
+    // titel:"RÉSISTANCE"
+    //   dis:"Kerastase Masque Thérapiste is a mask designed for very-damaged and over-processed hair, in need of strength and repair."
+    //  price:"700$"
     },
     {
       id:11,
       img:"https://cdn.shopify.com/s/files/1/0003/0736/3851/products/Untitleddesign_2.png?v=1656011331&width=550",
-    }
+  //  titel:"LIFELINE SKIN CARE"
+  //  dis:"Lifeline ProPlus Advanced Aqueous treatment is part of a new patented Biotech approach to Skin Therapy. Lifeline uses the world’s only proteins and peptides from human (non-fertilized) pluripotent stem cells, along with powerful concentrations of cutting edge ingredients, to make this a multi-tasking formulation for extremely effective results."
+  //  price:"150$"
+    },
   ]
